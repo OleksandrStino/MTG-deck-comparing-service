4b9c0274d8c58849e1e2162fc1ad1142a456cc31
stino_oleksandr <sashastinio@gmail.com>
2017-04-13 13:58:09 +0300

    Added SpringSecurity config and OpenEntityManagerViewFilter

---
+++ src/main/java/com/mtg/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.mtg.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
